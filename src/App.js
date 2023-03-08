03972f984d800717ae7a6b36e1155ff5db46b8ee
ADSprogramer <isai2000a@hotmail.com>
2023-03-07 20:50:29 -0700

    fist step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import PageWrapper from './pageWrapper';
+import Paginacion from './Paginacion';
+import Pelicula from './Movie';
+// import peliculasJson from './api';
 
 function App() {
+
+  const [paginaActual, setPaginaActual] = useState(1);
+  const [peliculas, setPeliculas] = useState([]);
+  const TOTAL_POR_PAGINA = 7;
+
+  useEffect(() => {
+    anyApi();
+  }, []);
+
+  // let peliculas = peliculasJson;
+
+  const anyApi = async () => {
+
+    // https://cors-anywhere.herokuapp.com/
+
+    let response = await fetch('https://raw.githubusercontent.com/jaito789/json/main/peliculas.json', {
+        method: 'GET',
+        // mode: 'no-cors',
+        headers: {
+            // "Accept": 'aplication/json',
+            // "Content-type": 'aplication/json',
+            // "origin": 'https://www.omdbapi.com/?i=tt3896198&apikey=7f1fd26d'
+        }
+    });
+
+    let json = await response.json();
+
+    setPeliculas(json);
+
+  }
+
+  const getTotalPaginas = () => {
+    let cantidadTotalDePeliculas = peliculas.length;
+    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
+  }
+
+  let slicePeliculas = peliculas.slice(
+    (paginaActual - 1) * TOTAL_POR_PAGINA,
+    paginaActual * TOTAL_POR_PAGINA
+);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+
+      {slicePeliculas.map(peliculas =>
+        <Pelicula titulo={peliculas.titulo} calificacion={peliculas.calificacion}
+          director={peliculas.director} actores={peliculas.actores} fecha={peliculas.fecha_lanzamiento} duracion={peliculas.duracion}
+          img={peliculas.img}>
+          {peliculas.descripcion}
+        </Pelicula>
+      )}
+
+      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
+        setPaginaActual(pagina)
+      }} />
+
+    </PageWrapper>
   );
 }
 
